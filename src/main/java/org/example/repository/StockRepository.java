88990234342191654c6ef1ee39a29ca621dc4be2
Varga Petra <vargapetra0123@gmail.com>
2023-11-29 15:27:09 +0100

    Add StockRepository

---
+++ src/main/java/org/example/repository/StockRepository.java
@@ -0,0 +1,13 @@
+package org.example.repository;
+
+import org.example.model.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StockRepository extends JpaRepository<Stock, Integer> {
+
+    List<Stock> findAllByNameEqualsIgnoreCase(String name);
+}
