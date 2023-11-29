a019c58c22aeb3f55645e25698760b584b54bebf
Varga Petra <vargapetra0123@gmail.com>
2023-11-29 13:02:12 +0100

    Add Stock table

---
+++ src/main/java/org/example/model/Stock.java
@@ -0,0 +1,89 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "STOCK")
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "NAME")
+    private String Name;
+
+    @Column(name = "PRICE")
+    private int Price;
+
+    @Column(name = "QUANTITY")
+    private int Quantity;
+
+    public Stock() {
+    }
+
+    public Stock(int id, String name, int price, int quantity) {
+        this.id = id;
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getPrice() {
+        return Price;
+    }
+
+    public void setPrice(int price) {
+        Price = price;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        Quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Stock stock = (Stock) o;
+        return id == stock.id && Price == stock.Price && Quantity == stock.Quantity && Objects.equals(Name, stock.Name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, Name, Price, Quantity);
+    }
+
+    @Override
+    public String toString() {
+        return "Stock{" +
+                "id=" + id +
+                ", Name='" + Name + '\'' +
+                ", Price=" + Price +
+                ", Quantity=" + Quantity +
+                '}';
+    }
+}
