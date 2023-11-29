91bc7af86838fc0b201bbad8fdc47ba2cc544e0d
Varga Petra <vargapetra0123@gmail.com>
2023-11-29 13:31:22 +0100

    Add StockDto

---
+++ src/main/java/org/example/dto/StockDto.java
@@ -0,0 +1,77 @@
+package org.example.dto;
+
+import java.util.Objects;
+
+public class StockDto {
+
+    private int id;
+    private String name;
+    private int price;
+    private int quantity;
+
+    public StockDto(){
+
+    }
+
+    public StockDto(int id, String name, int price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StockDto stockDto = (StockDto) o;
+        return id == stockDto.id && price == stockDto.price && quantity == stockDto.quantity && Objects.equals(name, stockDto.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price, quantity);
+    }
+
+    @Override
+    public String toString() {
+        return "StockDto{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
