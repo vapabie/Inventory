384a3ef2fffcea483d6a4944ace557b546f209fa
Varga Petra <vargapetra0123@gmail.com>
2023-11-30 15:13:24 +0100

    change email to city

--- src/main/java/org/example/repository/StoreRepository.java
+++ src/main/java/org/example/repository/StoreRepository.java
@@ -9,5 +9,5 @@ import java.util.List;
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Integer> {
 
-    List<Store> findAllByEmailEqualsIgnoreCase(String email);
+    List<Store> findAllByCityEqualsIgnoreCase(String city);
 }
