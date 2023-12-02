bc36b00c8e871674e1c5dec0820d5496e2cb0f0a
Varga Petra <vargapetra0123@gmail.com>
2023-12-02 06:45:44 +0100

    declare ModelMapper Bean

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
@@ -9,4 +12,7 @@ public class Main {
 
         SpringApplication.run(Main.class, args);
     }
+
+    @Bean
+    public ModelMapper modelMapper(){return new ModelMapper();}
 }
