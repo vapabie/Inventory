2f72897d58e292b8d743e58b47ac1313ad31d348
Varga Petra <vargapetra0123@gmail.com>
2023-12-06 15:19:50 +0100

    create new store Service class called StoreServ

---
+++ src/main/java/org/example/service/StoreServ.java
@@ -0,0 +1,38 @@
+package org.example.service;
+
+import jakarta.transaction.Transactional;
+import org.example.model.Store;
+import org.example.repository.StoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class StoreServ {
+
+    private final StoreRepository storeRepository;
+
+    @Autowired
+    public StoreServ(StoreRepository storeRepository){
+        this.storeRepository = storeRepository;
+    }
+
+    public Store addStore(Store store){
+        return storeRepository.save(store);
+    }
+
+    public List<Store> findAllStore(){
+        return storeRepository.findAll();
+    }
+
+    public Store updateStore(Store store){
+        return storeRepository.save(store);
+    }
+
+    public void deleteStore(int id){
+        storeRepository.deleteStoreById(id);
+    }
+
+}
