06ac6128457eeba2e9baef0e8e4352be30e82c35
Varga Petra <vargapetra0123@gmail.com>
2023-12-06 15:19:04 +0100

    change List to Optional and add deleteStoreById

--- src/main/java/org/example/repository/StoreRepository.java
+++ src/main/java/org/example/repository/StoreRepository.java
@@ -4,10 +4,12 @@ import org.example.model.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+
+import java.util.Optional;
 
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Integer> {
 
-    List<Store> findAllByCityEqualsIgnoreCase(String city);
+    void deleteStoreById(int id);
+    Optional<Store> findAllById(int id);
 }
