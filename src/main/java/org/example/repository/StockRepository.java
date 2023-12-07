f5cc9fc08b26627710f03069d1665dd132489ee2
Varga Petra <vargapetra0123@gmail.com>
2023-12-07 11:29:52 +0100

    add deleteStockByid and change list to optional

--- src/main/java/org/example/repository/StockRepository.java
+++ src/main/java/org/example/repository/StockRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Integer> {
 
-    List<Stock> findAllByNameEqualsIgnoreCase(String name);
+    void deleteStockById(int id);
+
+    Optional<Stock> findAllById(int id);
 }
