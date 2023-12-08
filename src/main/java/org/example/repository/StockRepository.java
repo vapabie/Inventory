438532b320dd011544c3bcda399bf0b3867d0a72
Varga Petra <vargapetra0123@gmail.com>
2023-12-08 16:59:58 +0100

    add findByid

--- src/main/java/org/example/repository/StockRepository.java
+++ src/main/java/org/example/repository/StockRepository.java
@@ -1,6 +1,7 @@
 package org.example.repository;
 
 import org.example.model.Stock;
+import org.example.model.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +12,9 @@ import java.util.Optional;
 public interface StockRepository extends JpaRepository<Stock, Integer> {
 
     void deleteStockById(int id);
+
+    Optional<Stock> findById(int id);
+
 
     Optional<Stock> findAllById(int id);
 }
